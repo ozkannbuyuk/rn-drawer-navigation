0eb411efb0f661e20d4bad3446d4827c45410752
Özkan Büyük <ozkanbuyuk@icloud.com>
2024-02-26 02:44:42 +0300

    React Native - Drawer Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,58 +1,39 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { View, Text } from "react-native";
+import React from "react";
+import { Drawer } from "expo-router/drawer";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import CustomDrawerContent from "../components/customDrawerContent";
 
-import { useColorScheme } from '@/components/useColorScheme';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
+export default function _layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <Drawer
+      screenOptions={{
+        drawerLabelStyle: {
+          marginLeft: -20,
+        },
+      }}
+      drawerContent={CustomDrawerContent}
+    >
+      <Drawer.Screen
+        name="home"
+        options={{
+          drawerLabel: "Home",
+          title: "Home",
+          drawerIcon: ({ size, color }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="index"
+        options={{
+          drawerLabel: "About",
+          title: "About",
+          drawerIcon: ({ size, color }) => (
+            <Ionicons name="information-circle" size={size} color={color} />
+          ),
+        }}
+      />
+    </Drawer>
   );
 }
---
+++ app/home/notifications.tsx
@@ -0,0 +1,16 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { useNavigation } from "expo-router";
+import { DrawerActions } from "@react-navigation/native";
+
+export default function Notifications() {
+  const navigation = useNavigation();
+  const openDrawer = () => {
+    navigation.dispatch(DrawerActions.openDrawer());
+  };
+  return (
+    <View>
+      <Text>Notifications</Text>
+    </View>
+  );
+}
